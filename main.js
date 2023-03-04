439012410b7cb1e58f1cb4841578985413e33398
geek-nath <nath.codes@gmail.com>
2023-03-04 20:25:02 +0000

    used aos (animate on scroll), worked on facilities sect.

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const navbar = document.querySelector('nav');
 window.onscroll = ()=> {
     if(window.scrollY > 20) {
         navbar.classList.remove('border-top');
+        navbar.classList.add('shadow-sm');
     } else {
         navbar.classList.add('border-top');
+        navbar.classList.remove('shadow-sm');
     }
 }
